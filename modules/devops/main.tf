9550fdc5abda71d38d410a0a13baff84b769b647
Mrudul Lole <mrudullole08@gmail.com>
2025-12-20 16:48:40 +0530

    Update main.tf

--- modules/devops/main.tf
+++ modules/devops/main.tf
@@ -5,8 +5,10 @@ resource "aws_iam_role" "this" {
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action   = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
     }]
   })
 }
@@ -16,4 +18,3 @@ resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = var.policies[count.index]
 }
-
