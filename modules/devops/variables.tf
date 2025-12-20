29860a0947f91ed83ef9fe904e7300d428ab4d14
Mrudul Lole <mrudullole08@gmail.com>
2025-12-20 16:42:54 +0530

    Update variables.tf

--- modules/devops/variables.tf
+++ modules/devops/variables.tf
@@ -1,13 +1,10 @@
-variable "role_name" {
-  default = "devops-role"
-}
-
 variable "policies" {
   default = [
-    "arn:aws:iam::aws:policy/AdministratorAccess",
-    "arn:aws:iam::aws:policy/AmazonEKSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
-    "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/CloudWatchFullAccess",
     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   ]
 }
