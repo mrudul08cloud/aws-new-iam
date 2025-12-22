afdc2e1c1cdf3d7d3b5fb6dd677ed42b95fe16c3
Mrudul Lole <mrudullole08@gmail.com>
2025-12-22 15:34:36 +0530

    Update main.tf

--- modules/dev/main.tf
+++ modules/dev/main.tf
@@ -1,14 +1,17 @@
-
 resource "aws_iam_role" "this" {
   name = var.role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "arn:aws:iam::718159740586:root" }
-      Action   = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::718159740586:root"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
