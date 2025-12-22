3d01185f8f68af57d53bad2af1f0ea8b090cafb0
Mrudul Lole <mrudullole08@gmail.com>
2025-12-22 15:50:07 +0530

    Update variables.tf

--- modules/data/variables.tf
+++ modules/data/variables.tf
@@ -11,3 +11,17 @@ variable "policies" {
     "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
   ]
 }
+
+variable "principal_type" {
+  description = "Principal type allowed to assume the role (AWS or Service)"
+  type        = string
+  default     = "Service"
+}
+
+variable "principal_value" {
+  description = "Principal value (AWS ARN or service name)"
+  type        = string
+  default     = "ec2.amazonaws.com"
+}
+
+
